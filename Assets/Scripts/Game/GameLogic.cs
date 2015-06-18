90b743b208e4e07272ceca3e184220c9fed9d178
ptx <ptx89@hotmail.com>
2015-06-18 22:42:21 +0200

    new file

---
+++ Assets/Scripts/Game/GameLogic.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameLogic : MonoBehaviour 
+{
+    static public GameLogic instance;
+    void Awake() { instance = this; }
+    void OnDestroy() { instance = null; }
+
+    // State of the game
+    public GameState gameState = GameState.UNCONNECTED;
+
+    public enum GameState
+    {
+        UNCONNECTED,
+        LOBBY,
+        INGAME
+    }
+
+    public List<NetPlayer> playerList = new List<NetPlayer>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Menu/Screen/UIScreenServer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScreenServer : MonoBehaviour, IMenuScreen
+{
+    public UIChatInput uiChatInput;
+
+    public void Show() { }
+
+    public void Hide() { }
+
+}

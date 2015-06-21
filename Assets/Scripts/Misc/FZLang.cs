0120b93fd93a887367f7b5e8d4907d1556ce63da
ptx <ptx89@hotmail.com>
2015-06-21 17:51:18 +0200

    Added new strings

---
+++ Assets/Scripts/Misc/FZLang.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class FZLang 
+{
+    public static string lobby_EmptySlotName = "-";
+
+    public static string chat_ColorConsole = "[FCCE8E]";
+
+    public static string chat_ColorText = "[FFFFFF]";
+    public static string chat_ColorLocalName = "[FB845C]";
+    public static string chat_ColorLocalTimestamp = "[FB845C]";
+    public static string chat_ColorRemoteName = "[FCCE8E]";
+    public static string chat_ColorRemoteTimestamp = "[FCCE8E]";
+
+    public static string chat_ColorServerName = "[EE12AC]";
+    public static string chat_ColorServerTimestamp = "[EE12AC]";
+}

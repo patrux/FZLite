07bd725f0cf8229ec1001ca2c7981c71f977702e
ptx <ptx89@hotmail.com>
2015-07-18 13:25:09 +0200

    Changed colors

--- Assets/Scripts/Misc/FZLang.cs
+++ Assets/Scripts/Misc/FZLang.cs
@@ -1,18 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// String look-up for names and colors.
+/// </summary>
 public static class FZLang 
 {
+    /// <summary>
+    /// Names
+    /// </summary>
     public static string lobby_EmptySlotName = "-";
 
-    public static string chat_ColorConsole = "[FCCE8E]";
+    /// <summary>
+    /// Colors
+    /// </summary>
+    public static string chat_ColorConsole = "[F1A242]";
 
     public static string chat_ColorText = "[FFFFFF]";
+
     public static string chat_ColorLocalName = "[FB845C]";
     public static string chat_ColorLocalTimestamp = "[FB845C]";
+
     public static string chat_ColorRemoteName = "[FCCE8E]";
     public static string chat_ColorRemoteTimestamp = "[FCCE8E]";
 
-    public static string chat_ColorServerName = "[EE12AC]";
-    public static string chat_ColorServerTimestamp = "[EE12AC]";
+    public static string chat_ColorServerName = "[F14266]";
+    public static string chat_ColorServerTimestamp = "[F14266]";
 }

7d5f256ce3100c12de66844bceb39f35cb2707c3
patrux <ptx89@hotmail.com>
2015-12-20 15:50:18 +0100

    Update

---
+++ Assets/Scripts/Game/AbilitySystem/Ability.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+// http://forum.unity3d.com/threads/skill-casting-method-and-secondary-effects.279190/#post-1844525
+
+public class Ability
+{
+    private AbilityInfo abilityInfo;
+    private List<AbilityBehaviors> behaviours;
+    //private GameObject particleEffect; // assign on ability creation
+
+    private bool requiresTarget; // barrier, tos, ns etc
+    private bool canSelfCast;
+    private float cooldown;
+    private float castTime;
+    private float energyCost;
+    private AbilityType abilityType;
+
+    public enum AbilityType
+    {
+        Spell,
+        Melee,
+        Buff,
+        Debuff
+    }
+
+    public Ability(AbilityInfo _abilityInfo, List<AbilityBehaviors> _behaviors)
+    {
+        abilityInfo = _abilityInfo;
+        behaviours = new List<AbilityBehaviors>();
+        behaviours = _behaviors;
+        cooldown = 0f;
+        castTime = 1f;
+
+        requiresTarget = false;
+        canSelfCast = false;
+    }
+
+    public Ability(AbilityInfo _abilityInfo, List<AbilityBehaviors> _behaviors, bool _requiresTarget, bool _canSelfCast, float _cooldown, float _castTime, float _energyCost)
+    {
+        abilityInfo = _abilityInfo;
+        behaviours = new List<AbilityBehaviors>();
+        behaviours = _behaviors;
+        cooldown = _cooldown;
+        castTime = _castTime;
+        energyCost = _energyCost;
+        requiresTarget = _requiresTarget;
+        canSelfCast = _canSelfCast;
+    }
+
+    public float AbilityCooldown
+    {
+        get { return cooldown; }
+    }
+
+    public float AbilityCastTime
+    {
+        get { return castTime; }
+    }
+
+    public float AbilityEnergyCost
+    {
+        get { return energyCost; }
+    }
+
+    public AbilityInfo AbilityInfo
+    {
+        get { return abilityInfo; }
+    }
+
+    public List<AbilityBehaviors> AbilityBehaviours
+    {
+        get { return behaviours; }
+    }
+
+    /// <summary>
+    /// Call OnUse
+    /// </summary>
+    public void UseAbility()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Game/Sound/SoundManager.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+
+    public enum SoundEffect
+    {
+        PLAYERHIT,
+        fireball01,
+        fireball02,
+        fireball03
+    }
+    
+    public AudioSource PlaySoundAttached(Vector3 _position, SoundEffect _soundEffect, GameObject _parent)
+    {
+        AudioSource audioSource = InstantiateAudioSource(_position, _soundEffect);
+        audioSource.gameObject.transform.parent = _parent.transform;
+        return audioSource;
+    }
+
+    public AudioSource PlaySoundAtPosition(Vector3 _position, SoundEffect _soundEffect)
+    {
+        AudioSource audioSource = InstantiateAudioSource(_position, _soundEffect);
+        return audioSource;
+    }
+
+    public void RandomPitch(AudioSource _audioSource, float _min, float _max)
+    {
+        _audioSource.pitch = Random.Range(_min, _max);
+    }
+
+    public void RandomVolume(AudioSource _audioSource, float _min, float _max)
+    {
+        _audioSource.volume = Random.Range(_min, _max);
+    }
+
+    public void RandomVariation(AudioSource _audioSource, string _baseName, int _maxIndex)
+    {
+        _audioSource.Stop();
+        int rng = Random.Range(0, _maxIndex);
+        _audioSource.clip = GetAudioClip(_baseName + rng.ToString());
+        _audioSource.Play();
+    }
+
+    /// <summary>
+    /// Instantiates a new AudioSource.
+    /// </summary>
+    AudioSource InstantiateAudioSource(Vector3 _position, SoundEffect _soundEffect)
+    {
+        GameObject go = (GameObject)Instantiate(Resources.Load("Sound/SoundFx"), _position, Quaternion.identity);
+        AudioSource audioSource = go.GetComponent<AudioSource>();
+        audioSource.clip = GetAudioClip(_soundEffect.ToString());
+        audioSource.Play();
+        go.GetComponent<SoundDestroyFinished>().enabled = true;
+        return audioSource;
+    }
+
+    /// <summary>
+    /// Gets an AudioClip.
+    /// </summary>
+    AudioClip GetAudioClip(string _soundEffect)
+    {
+        return (Resources.Load("Sound/Sounds/" + _soundEffect) as AudioClip);
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}

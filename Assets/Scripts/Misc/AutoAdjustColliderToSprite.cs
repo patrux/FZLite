4aaeaf0d2df03de3cfc24d22be6922819923cc5f
patrux <ptx89@hotmail.com>
2015-12-28 12:52:22 +0100

    Added

---
+++ Assets/Scripts/Misc/AutoAdjustColliderToSprite.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoAdjustColliderToSprite : MonoBehaviour 
+{
+	void Awake () 
+	{
+        BoxCollider collider = GetComponent<BoxCollider>();
+        UISprite sprite = gameObject.transform.Find("Sprite").GetComponent<UISprite>();
+        collider.center = new Vector3(sprite.transform.localPosition.x, sprite.transform.localPosition.y, 0f);
+        collider.size = new Vector3(sprite.localSize.x, sprite.localSize.y, 1f);
+        Destroy(this);
+    }
+}
